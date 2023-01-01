e70c8e8e43435204428723c46aeeee902447d9e8
Paul Carroll <paul.x.carroll@questdiagnostics.com>
2022-12-31 17:56:13 -0800

    Finish part 2

--- day10/part1.py
+++ day10/part1.py
@@ -1,6 +1,8 @@
 cycle = 0
 reg = 1
 signals = []
+crt = "\n"
+pixel = 0
 
 
 def check_interesting(cycle: int, reg: int, signals: list[int]) -> None:
@@ -22,8 +24,23 @@ with open("input.txt") as f:
 
         for _ in range(inc):
             cycle += 1
-            check_interesting(cycle, reg, signals)
+
+            # If the sprite position (register plus and minus 1) equals the cycle, draw a #, else draw a .
+            # Every 40 cycles add a new line
+            if cycle % 40 == 20:
+                signals.append(cycle * reg)
+
+            if pixel in [reg - 1, reg, reg + 1]:
+                crt += "#"
+
+            else:
+                crt += "."
+
+            pixel += 1
+            if cycle % 40 == 0:
+                crt += "\n"
+                pixel = 0
 
         reg += amt
 
-print(sum(signals))
+print(crt)
