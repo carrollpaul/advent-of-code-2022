9de28cd16773afc1188950288ce4bfb7bf1b3440
Paul Carroll <paul.x.carroll@questdiagnostics.com>
2023-01-01 20:22:59 -0500

    update

--- day10/part1.py
+++ day10/part1.py
@@ -4,39 +4,31 @@ signals = []
 crt = "\n"
 pixel = 0
 
-
-def check_interesting(cycle: int, reg: int, signals: list[int]) -> None:
-
-    for val in [20, 60, 100, 140, 180, 220]:
-        if cycle == val:
-            signals.append(cycle * reg)
-
-
 with open("input.txt") as f:
     for line in f.readlines():
         match line.split():
             case ["noop"]:
-                inc = 1
-                amt = 0
+                inc = 1  # Number of cycles to process
+                amt = 0  # Amount to add to register after cycles processed
             case "addx", amt:
                 inc = 2
                 amt = int(amt)
 
         for _ in range(inc):
-            cycle += 1
+            cycle += 1  # Start of cycle
 
-            # If the sprite position (register plus and minus 1) equals the cycle, draw a #, else draw a .
-            # Every 40 cycles add a new line
             if cycle % 40 == 20:
                 signals.append(cycle * reg)
 
+            # If one of the three sprite positions (register plus and minus 1) is at target pixel, draw a #, else draw a .
             if pixel in [reg - 1, reg, reg + 1]:
                 crt += "#"
-
             else:
                 crt += "."
 
-            pixel += 1
+            pixel += 1  # End of cycle
+
+            # Every 40 cycles add a new line and reset the pixel to 0
             if cycle % 40 == 0:
                 crt += "\n"
                 pixel = 0
